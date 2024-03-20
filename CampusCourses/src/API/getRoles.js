291e6c05344a539b9ba8c1986363b0799eff7cb9
Vasandr15 <vasandrey007@gmail.com>
2024-03-20 23:59:10 +0700

    main page done

---
+++ CampusCourses/src/API/getRoles.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+export const getRoles = async () => {
+    try {
+        let response = await axios.get('https://camp-courses.api.kreosoft.space/roles',
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            });
+        console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.log(error)
+    }
+}
